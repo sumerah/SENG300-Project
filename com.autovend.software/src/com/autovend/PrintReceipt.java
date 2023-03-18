fd27a977c4863bf519dad8e34105395221113aaf
Farhan2275 <128016858+Farhan2275@users.noreply.github.com>
2023-03-18 04:49:25 -0600

    Update PrintReceipt.java

--- com.autovend.software/src/com/autovend/PrintReceipt.java
+++ com.autovend.software/src/com/autovend/PrintReceipt.java
@@ -1,7 +1,21 @@
 package com.autovend;
+
 import com.autovend.*;
 import com.autovend.devices.*;
+import com.autovend.devices.observers.AbstractDeviceObserver;
+import com.autovend.devices.observers.ElectronicScaleObserver;
+import com.autovend.devices.observers.ReceiptPrinterObserver;
 
-public class PrintReceipt implements AbstractDeviceObserver<???>{
+
+
+public class PrintReceipt implements ElectronicScaleObserver, ReceiptPrinterObserver {
+	private static final long serialVersionUID = 2485932101191989634L;
+	private int charactersOfInkRemaining = 0;
+	
+
+	public void print() {
+		Bill bill = new Bill(value, currency);
+	}
 	
 }
+
