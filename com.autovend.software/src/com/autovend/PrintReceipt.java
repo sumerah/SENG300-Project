7b23667706fa7572a98b50f5284addbbfef04d6f
Farhan2275 <128016858+Farhan2275@users.noreply.github.com>
2023-03-18 05:57:23 -0600

    Update PrintReceipt.java

--- com.autovend.software/src/com/autovend/PrintReceipt.java
+++ com.autovend.software/src/com/autovend/PrintReceipt.java
@@ -1,3 +1,4 @@
+
 package com.autovend;
 
 import com.autovend.*;
@@ -12,10 +13,37 @@ public class PrintReceipt implements ElectronicScaleObserver, ReceiptPrinterObse
 	private static final long serialVersionUID = 2485932101191989634L;
 	private int charactersOfInkRemaining = 0;
 	
+	public static final int MAXIMUM_INK = 1 << 20;
 
-	public void print() {
-		Bill bill = new Bill(value, currency);
+	public void ScanReceipt(SelfCheckoutLogic scl) {
+		scannedItems = scl.scannedItems;
 	}
 	
-}
+	public void print() {
+		Bill bill = new Bill(value, currency);
+		int value = bill.getValue();
+		Currency currency = bill.getCurrency;
+		for (Bill bill : scannedItems) {
+			value = bill.getValue();
+			String itemName = bill.getDesccription();
+		}
+		//BarcodedProduct item = ProductDatabases.BARCODED_PRODUCT_DATABASE.get(barcode);
+	}
+	
+	
+	// Exception thrown if the printer runs out of ink
+	public void addInk(int quantity) throws OverloadException {
+		if(quantity < 0)
+			throw new SimulationException("Are you trying to remove ink?");
 
+		if(charactersOfInkRemaining + quantity > MAXIMUM_INK)
+			throw new OverloadException("You spilled a bunch of ink!");
+
+		if(quantity > 0) {
+			charactersOfInkRemaining += quantity;
+
+			for(ReceiptPrinterObserver observer : observers)
+				observer.reactToInkAddedEvent(this);
+		}
+	}
+}
